e38a59bcb457ff89ae6bec023f8933a1d9392f37
Luckyoranges <54844457+Luckyoranges@users.noreply.github.com>
2020-09-17 09:05:07 -0500

    去除异常测试，读入空文本时相似度为0.00

--- main.py
+++ main.py
@@ -4,24 +4,25 @@ import Features
 import Similarity
 import sys
 
-try:
-    wordlist_s = TextPreprocess.Tokens(sys.argv[1])
-    wordlist_e = TextPreprocess.Tokens(sys.argv[2])
 
-    dict_s = Process.DictBiulder(wordlist_s)
-    dict_e = Process.DictBiulder(wordlist_e)
+wordlist_s = TextPreprocess.Tokens(sys.argv[1])
+wordlist_e = TextPreprocess.Tokens(sys.argv[2])
 
-    Fin_dict = Process.MergeDict(dict_s, dict_e)
+dict_s = Process.DictBiulder(wordlist_s)
+dict_e = Process.DictBiulder(wordlist_e)
 
-    V1 = Features.GetVector(dict_s, Fin_dict)
-    V2 = Features.GetVector(dict_e, Fin_dict)
+Fin_dict = Process.MergeDict(dict_s, dict_e)
 
-    ans = Similarity.CosineSimilarity(V1, V2)
-    with open(sys.argv[3], 'w') as f_obj:
+V1 = Features.GetVector(dict_s, Fin_dict)
+V2 = Features.GetVector(dict_e, Fin_dict)
+ans = Similarity.CosineSimilarity(V1, V2)
+
+with open(sys.argv[3], 'w') as f_obj:
+    if ans == 0:
+        f_obj.write("0.00")
+    else:
         temp = str(ans)
         contents = ''
         for i in range(0, 4):
             contents = contents + temp[i]
         f_obj.write(contents)
-except IndexError:
-    print("HeHeHe, 读入了空文件吧！")
